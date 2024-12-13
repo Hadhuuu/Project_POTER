d564668830e3cdbc90019b96018bd6271e9d5763
SeptianTito123 <122253552+SeptianTito123@users.noreply.github.com>
2024-12-13 10:27:23 +0700

    Add files via upload

--- dosen/get_pelanggaran.php
+++ dosen/get_pelanggaran.php
@@ -13,16 +13,24 @@ $dosen_id = $_SESSION['user_id'];  // ID Dosen yang login
 // Jika ada filter status
 $status = isset($_GET['status']) ? $_GET['status'] : '';
 
-$sql = "SELECT p.*, m.nim, m.nama AS nama_mahasiswa, d.nama AS nama_dosen, 
-jp.tingkatan AS tingkatan_pelanggaran, js.keterangan AS sanksi 
-FROM pelanggaran p 
-JOIN mahasiswa m ON p.id_mahasiswa = m.id 
+$sql = "
+SELECT p.*, 
+       m.nim, 
+       m.nama AS nama_mahasiswa, 
+       d.nama AS nama_dosen, 
+       jp.tingkatan AS tingkatan_pelanggaran, 
+       js.keterangan AS sanksi 
+FROM pelanggaran p
+JOIN mahasiswa m ON p.id_mahasiswa = m.id
 JOIN dosen d ON p.id_pelapor = d.id
 JOIN jenis_pelanggaran jp ON p.tingkatan_pelanggaran = jp.id
 JOIN jenis_sanksi js ON p.id_sanksi = js.id
-WHERE m.id_kelas = (
-SELECT id_kelas FROM kelas WHERE id_dpa = ?
+WHERE m.id_kelas IN (
+    SELECT id 
+    FROM kelas 
+    WHERE id_dpa = ?
 )";
+
 
 
 // Menambahkan filter status jika ada
